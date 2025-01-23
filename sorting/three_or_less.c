63a1561e0e9e8c4b1132c3ec42d4ada4848f758e
Alisa Valiullina <avaliull@student.codam.nl>
2025-01-23 01:21:34 +0100

    an edit to conform to type change

--- sorting/three_or_less.c
+++ sorting/three_or_less.c
@@ -7,8 +7,8 @@ void	sort_two(t_stacks *stacks, t_intlist *sorted_stack, e_op_stack a_b)
 	
 	if (el1 > el2)
 		do_op(stacks, op_swap, a_b, 1);
-	sorted_stack->sorted = yes;
-	sorted_stack->next->sorted = yes;
+	sorted_stack->sorted = true;
+	sorted_stack->next->sorted = true;
 }
 
 void	sort_three(t_stacks *stacks, t_intlist *sorted_stack, e_op_stack a_b)
@@ -19,9 +19,9 @@ void	sort_three(t_stacks *stacks, t_intlist *sorted_stack, e_op_stack a_b)
 	
 	if (el1 < el2 && el2 < el3)
 	{
-		sorted_stack->sorted = yes;
-		sorted_stack->prev->sorted = yes;
-		sorted_stack->next->sorted = yes;
+		sorted_stack->sorted = true;
+		sorted_stack->prev->sorted = true;
+		sorted_stack->next->sorted = true;
 		return ;
 	}
 	if (el1 < el2 && el2 > el3)
@@ -31,9 +31,9 @@ void	sort_three(t_stacks *stacks, t_intlist *sorted_stack, e_op_stack a_b)
 	if ((el1 < el2 && el2 > el3 && el3 > el1) || (el1 > el2 && el2 > el3)
 			|| (el1 > el2 && el2 < el3 && el3 > el1))
 		do_op(stacks, op_swap, a_b, 1);
-	sorted_stack->sorted = yes;
-	sorted_stack->prev->sorted = yes;
-	sorted_stack->next->sorted = yes;
+	sorted_stack->sorted = true;
+	sorted_stack->prev->sorted = true;
+	sorted_stack->next->sorted = true;
 }
 
 void	sort_small_stack(t_stacks *stacks, e_op_stack op_stack, int len)
@@ -46,7 +46,7 @@ void	sort_small_stack(t_stacks *stacks, e_op_stack op_stack, int len)
 		sorted_stack = stacks->b;
 	if (len == 1)
 	{
-		sorted_stack->sorted = yes;
+		sorted_stack->sorted = true;
 		return ;
 	}
 	else if (len == 2)
