e2ad779694a76d7e2aaab48aaa11d55f5fa88ee2
Alisa Valiullina <avaliull@f0r2s5.codam.nl>
2025-02-12 13:43:40 +0100

    gentest wee

--- gen_testset.sh
+++ gen_testset.sh
@@ -6,10 +6,4 @@ then
 fi
 touch input.txt
 shuf -i "$range" -n $amount >> input.txt
-#i=0
-#while [ $i -lt $amount ]
-#do
-#    shuf -i "$range" -n 1 >> input.txt
-#	true $((i=i+1))
-#done
 sed -i ':a;N;$!ba;s/\n/ /g' input.txt
